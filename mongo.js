00ff44080469e23a4fe7c61f49f396549dbb1f6e
Omar Tejada <omar14tejada@gmail.com>
2024-10-25 17:31:15 -0400

    updating url base.

--- mongo.js
+++ mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url = 
-'mongodb+srv://fullstack:FSOTest1@cluster0.sfsuf.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0'
+'mongodb+srv://fullstack:<db_password>@cluster0.sfsuf.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.set('strictQuery', false)
 
