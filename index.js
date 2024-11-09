7322e51d3a40beb2245514c97f3c445ce84602ed
Omar Tejada <omar14tejada@gmail.com>
2024-11-08 22:30:15 -0500

    fso-part3: d; updating error handler and validation,

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
 
   next(error);
@@ -44,14 +46,8 @@ app.get("/api/notes", (request, response) => {
 });
 
 // CREATE
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
 
   const note = new Note({
     content: body.content,
@@ -60,7 +56,8 @@ app.post("/api/notes", (request, response) => {
 
   note.save().then((savedNote) => {
     response.json(savedNote);
-  });
+  })
+  .catch(error => next(error))
 });
 
 // GET BY ID
@@ -87,14 +84,13 @@ app.delete("/api/notes/:id", (request, response, next) => {
 
 // UPDATE
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const {content, important} = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    {content, important},
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
